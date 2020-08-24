0220db9ff420192cd51b73702de236208af00eda
Isaiah C <iboycord@yahoo.com>
2020-08-24 01:15:23 -0400

    Small work on refactoring Item script to be more in line with the move script.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -8,22 +8,63 @@ public class Item : ScriptableObject
     new public string name = "New Item";
     public Sprite icon = null;
     public bool isDefaultItem = false;
-    public itemType item_type = itemType.key;
+    public itemType item_type;
+    public bool isKeyItem;
 
-    public useType use_type = useType.NoLimit;
+    public bool infiniteUses;
     public int useNum = 0;
+
+    public Move move;
+    public int healAmount;
+    public StatusEffectList statusApplied;
 
     public void Awake()
     {
-        if(use_type == useType.NoLimit){
-            useNum = 0;
-        }
+        useNum = Mathf.Clamp(useNum, 0, int.MaxValue);
     }
 
+    // For equipment
     public virtual void Use()
     {
         Debug.Log("Using " + name);
+    }
 
+    // For useable items
+    public virtual void Use(CharacterStats user, CharacterStats target)
+    {
+        Debug.Log("Using " + name);
+        switch (item_type)
+        {
+            case itemType.attack:
+                move.Use(user, target);
+                UseIncrementer(-1);
+                break;
+            case itemType.heal:
+                user.Heal(healAmount);
+                UseIncrementer(-1);
+                break;
+            case itemType.status:
+                StatusEffectHandler userSEH = user.gameObject.GetComponent<StatusEffectHandler>();
+                userSEH.AssignStatus(statusApplied);
+                break;
+            case itemType.none:
+                Debug.Log("This item has no effect...");
+                break;
+        }
+        
+    }
+
+    public virtual void UseIncrementer(int uses)
+    {
+        if (!infiniteUses || isKeyItem)
+        {
+            useNum -= uses;
+            if (useNum <= 0)
+            {
+                RemoveFromInventory();
+            }
+        }
+        
     }
 
     public void RemoveFromInventory()
@@ -32,5 +73,4 @@ public class Item : ScriptableObject
     }
 }
 
-public enum itemType { heal, attack, status, key }
-public enum useType { singleUse, MultiUse, NoLimit }
+public enum itemType { heal, attack, status, none }
