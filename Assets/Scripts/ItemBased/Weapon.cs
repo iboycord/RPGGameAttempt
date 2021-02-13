9b026f92302c1e50061a138c16c8d2b3da84dd7f
Isaiah C <iboycord@yahoo.com>
2021-02-13 01:23:24 -0500

    Just weapon thoughts. Going to section off weapons based on how they animate in relation to each other and if the unit can use them.

--- Assets/Scripts/ItemBased/Weapon.cs
+++ Assets/Scripts/ItemBased/Weapon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Weapon")]
 public class Weapon : Item
 {
+    public WeaponTypes type;
     public MoveNumHolder[] WeapMoveset;
 
     public override void Use()
@@ -19,5 +20,20 @@ public class Weapon : Item
     {
         if (item_type != ItemType.weapon) { return; }
         EquipmentManager.instance.Unequip();
+    }
+
+    public enum WeaponTypes
+    {
+        Blade, // Sword, dagger, rapier, ext
+        Stave_based, // Lance, halberd, staves, rods ext
+        Heavy, // Axe, hammer, club thing
+        Brawler, // gauntlets and sports tape
+        Gun, // Gun
+        Shield, // Aegis, buckler, ext
+
+        Tome,
+        Bow,
+        Ring // Ring Con accessory.
+
     }
 }
